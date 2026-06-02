eb8bd9e447f302c5717f9dad1e8b83674b65d740
yatothedeveloper <yahyamagdy986@gmail.com>
2026-06-02 18:54:19 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+/*
+ * Project: [اكتب اسم مشروعك هنا]
+ * Author: Yahya Magdy
+ * License: CC BY-NC 4.0 (Non-Commercial Use Only)
+ * 
+ * يمنع استخدام هذا الكود لأغراض تجارية أو بيعه.
+ * كافة الحقوق محفوظة © 2026 Yahya Magdy.
+ */
 document.addEventListener("DOMContentLoaded", () => {
         const pingList = document.querySelector(".ping-list");
         
